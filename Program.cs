2107e596cb3a287dc0b1b5341f83120ca2f8e32b
Zachary Leonard <zachary.leonard@faithlife.com>
2019-07-15 17:36:12 -0700

    Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dot_experiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
